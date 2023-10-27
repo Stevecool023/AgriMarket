e66f5a0405eb7d6025264dfbf1b9c76cb7eba566
root <root@DESKTOP-NS4QK55>
2023-10-27 19:04:15 +0300

    init storage

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -2,7 +2,15 @@
 
 """ Module to create a unique FileStorage instance for the application """
 
-from models.engine.file_storage import FileStorage
+from os import getenv
+# from models.engine.file_storage import FileStorage
 
-storage = FileStorage()
+storage_t = getenv("SCOOL_TYPE_STORAGE")
+if storage_t == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
 storage.reload()
+
