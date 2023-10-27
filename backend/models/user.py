61403c77dd8dedfbe16f9b582d904dd7a1becd4c
root <root@DESKTOP-NS4QK55>
2023-10-27 11:50:31 +0300

    restructure

---
+++ backend/models/user.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+User class that inherits from BaseModel
+"""
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """ Class that defines attributes for a User """
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+    location = ""
+    phone_number = ""
