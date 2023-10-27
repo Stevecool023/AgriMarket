42401a1e6c8b05a786c0786496d74d6d828b68de
root <root@DESKTOP-NS4QK55>
2023-10-27 11:55:51 +0300

    restructure

--- models/management.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-# This is a placeholder file for managing classes that handle CRUD operations for Equipment, Products, Transations, Buyers, and Owners. The implementation of these classes depends on the framework used(e.g., Flask, Django). We should create classes or views that allow us to create, read, update, and delete records for our models.
-
-
-# The actual implementation of the management and view classes may vary depending on the web framework and tools used to build our agricultural website. We'll need to integrate these classes with our web framework to create a fully functional website.
--- models/views.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-# This is a placeholder file for views or controllers that handle the interaction between the website's users and the data models. We'll define routes, request handlers, and templates to display information to users and manage their interactions. The implementation of views depends on the web framework used. If we're using Flask, for example, we would define routes and request handlers in this file.
-
-
-# The actual implementation of the management and view classes may vary depending on the web framework and tools used to build the agricultural website. We'll need to integrate these classes with the web framework to create a fully functional website.
