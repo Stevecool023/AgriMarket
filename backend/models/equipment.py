f3e32f581596ad68898f52832b8cca8ece87161e
root <root@DESKTOP-NS4QK55>
2023-10-27 20:29:47 +0300

    equipment class

--- backend/models/equipment.py
+++ backend/models/equipment.py
@@ -4,15 +4,23 @@
 Equipment class that inherits from BaseModel
 """
 
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Float, Integer, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class Equipment(BaseModel):
+class Equipment(BaseModel, Base):
     """ Class that defines attributes for agricultural equipment """
-    name = ""
-    description = ""
-    quantity = 0
-    price = 0.0
-    condition = ""
-    location = ""
-    owner_id = ""
+    __tablename__ = 'equipment'
+
+
+    name = Column(String(128), nullable=False)
+    description = Column(String(256), nullable=True)
+    quantity = Column(Integer, nullable=False)
+    price = Column(Float, nullable=False)
+    condition = Column(String(128), nullable=True)
+    location = Column(String(128), nullable=True)
+    owner_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+
+    # Define a relationship to connect equipment with its owner (user)
+    owner = relationship('User', back_populates='equipment')
