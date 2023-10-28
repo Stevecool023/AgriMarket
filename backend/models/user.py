9807396d48399ec09ce4aa1a3cb7bfab35cee4ff
root <root@DESKTOP-NS4QK55>
2023-10-28 10:45:35 +0300

    user class inheriting from basemodel class

--- backend/models/user.py
+++ backend/models/user.py
@@ -4,25 +4,38 @@
 User class that inherits from BaseModel
 """
 
+import models
 from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
     """ Class that defines attributes for a User """
-    __tablename__ = 'users'
+    if models.storage_t == 'db':
+        __tablename__ = 'users'
 
 
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-    location = Column(String(128), nullable=True)
-    phone_number = Column(String(128), nullable=True)
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128),
+        last_name = Column(String(128), nullable=True)
+        location = Column(String(128), nullable=True)
+        phone_number = Column(String(128), nullable=True)
+        # Define a relationship to connect a user with their products
+        products = relationship('Product', backref='user')
+        equipment = relationship('Equipment', backref='user')
+        transactions = relationship('Transaction', backref='user', foreign_keys='Transaction.buyer_id')
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
+        location = ""
+        phone_number = ""
 
-    # Define a relationship to connect a user with their products
-    products = relationship('Product', back_populates='owner')
-    equipment = relationship('Equipment', back_populate='owner')
-    transactions_as_buyer = relationship('Transaction', back_populates='buyer', foreign_keys='Transaction.buyer_id')
-    transactions_as_owner = relationship('Transaction', back_populates='owner', foreign_keys='Transaction.owner_id')
+    def __init__(self, *args, **kwargs):
+        """ initializes user """
+        super().__init__(*args, **kwargs)
