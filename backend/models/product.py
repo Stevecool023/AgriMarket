59213cf987738a552a171107f6f2ace3b41d7a36
root <root@DESKTOP-NS4QK55>
2023-10-28 12:19:40 +0300

    product class

--- backend/models/product.py
+++ backend/models/product.py
@@ -11,15 +11,12 @@ from sqlalchemy.orm import relationship
 
 class Product(BaseModel, Base):
     """ Class that defines attributes for agricultural products """
-    __tablename__ = 'products'
+    if models.storage_t == 'db':
+        __tablename__ = 'products'
 
-    name = Column(String(128), nullable=False)
-    description = Column(String(256), nullable=True)
-    quantity = Column(Integer, nullable=False)
-    price = Column(Float, nullable=False)
-    category = Column(String(128), nullable=True)
-    location = Column(String(128), nullable=True)
-    owner_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-
-    # Define a relationship to connect a product with its seller (user)
-    owner = relationship('User', back_populates='products')
+        name = Column(String(128), nullable=False)
+        description = Column(String(256), nullable=True)
+        quantity = Column(Integer, nullable=False)
+        price = Column(Float, nullable=False)
+        category = Column(String(128), nullable=True)
+        location = Column(String(128), nullable=True)
